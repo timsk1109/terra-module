29a5ff6858a39898b0a3e1cdd91ce260d89730ff
Timothy <timothymang09@gmail.com>
2024-06-25 03:44:09 -0400

    my second commit with ingore

--- ec2-module.tf
+++ ec2-module.tf
@@ -1,3 +1,4 @@
-module "ec2-instance" {
-  source  = "terraform-aws-modules/ec2-instance/aws"
-  version = "5.6.1"
+# module "ec2-instance" {
+#   source  = "terraform-aws-modules/ec2-instance/aws"
+#   version = "5.6.1"
+# }
